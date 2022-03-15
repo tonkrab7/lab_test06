bdab7eb568b904ad08f675bfc595bb57e2d35768
tonkrab7 <tonkrab7@gmail.com>
2022-03-15 08:51:20 +0700

    init file lab_test06

---
+++ config/database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import ssl 
+from core import config
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+
+client = MongoClient("mongodb+srv://"+config.settings.user_name+":"+config.settings.pass_word+"@"+config.settings.host+"/myFirstDatabase?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+
+
+db = client.todo_app
+
+collection_name = db["todos_app"]
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from routes.years_route import years_api_router
+from routes.todos_route import todo_api_router
 
 app = FastAPI()
 
 app.include_router(years_api_router)
-
+app.include_router(todo_api_router)
